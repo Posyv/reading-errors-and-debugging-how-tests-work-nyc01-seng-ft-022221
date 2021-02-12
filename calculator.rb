1d9b83881b62db40b4b90c8eea69250410836249
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 18:27:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,23 @@
-def calculator
 first_number = 1.0
 second_number = 2.0
-end
 
-sum = first_number, second_number
+def sum = first_number , second_number
 first_number + second_number
 end
 sum
+
+def difference
+  first_number - second_number
+end 
+difference 
+
+def product
+  first_number * second_number
+end
+product
+
+def quotient
+first_number / second_number
+end
+quotient
+
