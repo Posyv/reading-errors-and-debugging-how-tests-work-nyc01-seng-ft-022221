ab54ed1124df16aedc18a12bd6169bcbae778704
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 18:30:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,21 +2,25 @@ first_number = 1.0
 second_number = 2.0
 
 def sum = first_number , second_number
-first_number + second_number
+sum = first_number.to_s + second_number.to_s
+puts sum
 end
-sum
+
 
 def difference
-  first_number - second_number
+  differnece = first_number - second_number
+  puts difference
 end
-difference
+
 
 def product
-  first_number * second_number
+multiplication =  first_number.to_s * second_number.to_s
+puts multiplication
 end
-product
+
 
 def quotient
-first_number / second_number
+quotient = first_number.to_s / second_number.to_s
+puts quotient
 end
-quotient
+
