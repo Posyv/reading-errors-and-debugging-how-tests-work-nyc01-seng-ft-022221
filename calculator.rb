b077562b75bc1b2ffa1ece4d69d24a6a9d4819f5
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 18:52:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,9 @@
 first_number = 1.0
 second_number = 2.0
 sum = first_number + second_number
+difference = first_number - second_number
 
 
-def difference (first_number, second_number)
-  differnece = first_number - second_number
-  puts difference
-end
 
 def product (first_number, second_number)
 multiplication =  first_number.to_s * second_number.to_s
