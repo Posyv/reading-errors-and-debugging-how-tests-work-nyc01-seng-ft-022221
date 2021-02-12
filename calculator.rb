68a10bc34dc215e8b132ffb9cf74dd21425a16b1
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 18:39:23 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,20 +6,17 @@ sum = first_number.to_s + second_number.to_s
 puts sum
 end
 
-
-def difference
+def difference (first_number, second_number)
   differnece = first_number - second_number
   puts difference
 end
 
-
-def product
+def product (first_number, second_number)
 multiplication =  first_number.to_s * second_number.to_s
 puts multiplication
 end
 
-
-def quotient
+def quotient (first_number, second_number)
 quotient = first_number.to_s / second_number.to_s
 puts quotient
 end
